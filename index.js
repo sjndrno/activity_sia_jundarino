7a5964ad038635d3cf75c7b79a33e5e774f03abe
Shaira Jundarino <shairam.jundarino@gmail.com>
2026-08-10 19:46:36 +0800

    feat(index): add student info

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+
+console.log("Student Information");
+console.log("Student ID: 202521055");
+console.log("Name: Shaira Jundarino");
+console.log("Course: BS Information Technology");
+console.log("Year Level: 2nd Year");
